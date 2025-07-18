d4d2d8fc4cf3f6cdc2bc271ec9f54ca938e2b89f
pigglehyun <sarah0661@pusan.ac.kr>
2025-07-18 11:24:33 +0900

    feat: add MemosByKeyword service logic

--- src/main/java/com/evcamp/memo/service/MemoService.java
+++ src/main/java/com/evcamp/memo/service/MemoService.java
@@ -58,9 +58,15 @@ public class MemoService {
         return id;
     }
 
+    public List<MemoResponseDto> getMemosByKeyword(String keyword) {
+        return memoRepository.findAllByKeywordOrderByModifiedAtDesc(keyword).stream().map(MemoResponseDto::new).toList();
+    }
+
     private Memo findMemo(Long id) {
         return memoRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("선택한 메모는 존재하지 않습니다.")
         );
     }
+
+
 }
