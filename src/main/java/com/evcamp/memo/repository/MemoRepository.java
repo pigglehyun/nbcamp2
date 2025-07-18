04421b2335e39d66334bcb4a6d6793f2862a4f2f
pigglehyun <sarah0661@pusan.ac.kr>
2025-07-18 11:24:14 +0900

    feat: add MemosByKeyword JPQL

--- src/main/java/com/evcamp/memo/repository/MemoRepository.java
+++ src/main/java/com/evcamp/memo/repository/MemoRepository.java
@@ -3,6 +3,8 @@ package com.evcamp.memo.repository;
 import com.evcamp.memo.entity.Memo;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,4 +12,13 @@ import java.util.List;
 @Repository
 public interface MemoRepository extends JpaRepository<Memo, Long> {
     List<Memo> findAllByOrderByModifiedAtDesc();
+
+    /**
+     *
+     * @param keyword : 찾고자 하는 특정 키워드
+     * 키워드와 '일치'하는 것이 아니라 '포함'되어야 하는 것이기 때문에 '%keyword%'형식으로 작성했고, concat 함수를 통해 JPQL에 적용
+     *
+     */
+    @Query("select m from Memo m where m.contents like concat('%',:keyword,'%') order by m.modifiedAt desc ")
+    List<Memo> findAllByKeywordOrderByModifiedAtDesc(@Param("keyword")String keyword);
 }
